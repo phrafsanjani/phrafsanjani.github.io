1555dd3ebac4f8d7d8a40b461366ed42eb673477
phrafsanjani <64379572+phrafsanjani@users.noreply.github.com>
2025-09-14 05:28:16 +0000

    Deploying to gh-pages from  @ d9d1d0e40800cda2193ac4bbb006783ed514efad 🚀

--- assets/scripts/bookcard.js
+++ assets/scripts/bookcard.js
@@ -4,6 +4,12 @@ function toggleContent(src) {
     const reviewCard = document.querySelector(`#${buttonId} .review-card`);
 
     reviewCard.style.height = `${normalCard.offsetHeight}px`;
+
+    if (src.textContent === "My Review") {
+        src.textContent = "Back";
+    } else {
+        src.textContent = "My Review";
+    }
     
     reviewCard.classList.toggle('visible');
     normalCard.classList.toggle('hidden');
